81e9a683afebb60fad792ad456c763afb1b70437
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-07-23 12:38:26 -0500

    update connect.js

--- connect.js
+++ connect.js
@@ -1,24 +1,25 @@
-import Sequelize from 'sequelize';
+import Sequelize from 'sequelize'
 
 // Usa la URL de conexión proporcionada por Supabase desde las variables de entorno
 const sequelize = new Sequelize('postgres://postgres.sykdchrxmrtzehhuooeb:QuierolaMT09@aws-0-us-west-1.pooler.supabase.com:6543/postgres', {
-  dialectOptions: {
+    dialect: 'postgres',
+    dialectOptions: {
     ssl: {
-      require: true,
-      rejectUnauthorized: false
+        require: true,
+        rejectUnauthorized: false
     }
   }
-});
+})
 
 // Conexión a la base de datos por medio de Sequelize
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Conectado a la base de datos con éxito.')
-  })
-  .catch((error) => {
-    console.error('Error al conectar a la base de datos:', error)
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Conectado a la base de datos con éxito.')
+    })
+    .catch((error) => {
+        console.error('Error al conectar a la base de datos:', error)
+    })
 
-export default sequelize;
+export default sequelize
 
